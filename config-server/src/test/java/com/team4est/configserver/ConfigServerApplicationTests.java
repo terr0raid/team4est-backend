6324f12663f98c3495413dbf90652868a56de339
terr0raid <herakles13579@gmail.com>
2023-03-29 14:39:27 +0300

    config server implementation is completed

---
+++ config-server/src/test/java/com/team4est/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team4est.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
